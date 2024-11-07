01c75c927689c95be58a10d2016a3cdfcb72c4f2
Viki <hi@viki.moe>
2024-11-07 13:59:53 +0800

    chore: update deps

--- index.ts
+++ index.ts
@@ -1,47 +1,49 @@
-import ora from 'ora'
-import prompts, { type PromptObject } from 'prompts'
-import { getR5UpRoles, stripTags } from './utils'
-import { fetchPoolDetail, fetchPools } from './services'
+import ora from "ora";
+import prompts, { type PromptObject } from "prompts";
+import { getR5UpRoles, stripTags } from "./utils";
+import { fetchPoolDetail, fetchPools } from "./services";
 
-const loading = ora({ color: 'cyan' })
+const loading = ora({ color: "cyan" });
 
-loading.start('正在获取当前版本卡池列表...')
+loading.start("正在获取当前版本卡池列表...");
 
-const pools = await fetchPools()
-const poolDetails = await Promise.all(pools.map(e => fetchPoolDetail(e.gacha_id)))
+const pools = await fetchPools();
+const poolDetails = await Promise.all(
+	pools.map((e) => fetchPoolDetail(e.gacha_id)),
+);
 
-poolDetails.forEach(item => {
-  item.title = stripTags(item.title)
-  item.content = stripTags(item.content)
-})
-
-loading.succeed('当前版本卡池列表获取完毕')
-
-const questions: PromptObject[] = [
-  {
-    type: 'select',
-    name: 'pool',
-    message: '请选择卡池',
-    choices: poolDetails.map(e => ({
-      title: `${e.title} - ${getR5UpRoles(e)}`,
-      value: e.title
-    }))
-  },
-  {
-    type: 'number',
-    name: 'count',
-    message: '请输入抽卡次数'
-  }
-]
-
-const { pool, count } = await prompts(questions)
-const p = poolDetails.find(e => e.title === pool)
-
-const status = {
-  totalCount: count,
-  currentCount: 0,
-  isNextR5Up: false,
-  isNextR4Up: false
+for (const item of poolDetails) {
+	item.title = stripTags(item.title);
+	item.content = stripTags(item.content);
 }
 
-console.log(pool, count)
+loading.succeed("当前版本卡池列表获取完毕");
+
+const questions: PromptObject[] = [
+	{
+		type: "select",
+		name: "pool",
+		message: "请选择卡池",
+		choices: poolDetails.map((e) => ({
+			title: `${e.title} - ${getR5UpRoles(e)}`,
+			value: e.title,
+		})),
+	},
+	{
+		type: "number",
+		name: "count",
+		message: "请输入抽卡次数",
+	},
+];
+
+const { pool, count } = await prompts(questions);
+const p = poolDetails.find((e) => e.title === pool);
+
+const status = {
+	totalCount: count,
+	currentCount: 0,
+	isNextR5Up: false,
+	isNextR4Up: false,
+};
+
+console.log(pool, count);
--- services.ts
+++ services.ts
@@ -1,83 +1,84 @@
-import got from 'got'
-import dayjs from 'dayjs'
-import isBetween from 'dayjs/plugin/isBetween'
-
-dayjs.extend(isBetween)
+import got from "got";
 
 /** 获取卡池列表，可选是否只返回当前卡池 */
 export async function fetchPools(isCurrent = true) {
-  const api =
-    'https://proxy.viki.moe/hk4e/gacha_info/cn_gf01/gacha/list.json?proxy-host=webstatic.mihoyo.com'
-  const res = await got.get(api).json<PoolRes>()
-  const { list = [] } = res?.data ?? {}
+	const api =
+		"https://webstatic.mihoyo.com/hk4e/gacha_info/cn_gf01/gacha/list.json";
+	const res = await got.get(api).json<PoolRes>();
+	const { list = [] } = res?.data ?? {};
 
-  if (!isCurrent) {
-    return list
-  }
+	if (!isCurrent) {
+		return list;
+	}
 
-  return list.filter(e => dayjs().isBetween(e.begin_time, e.end_time))
+	return list.filter((e) => {
+		const now = new Date().getTime();
+		const begin = new Date(e.begin_time).getTime();
+		const end = new Date(e.end_time).getTime();
+		return now >= begin && now <= end;
+	});
 }
 
 /** 通过卡池 id 获取详细信息 */
 export async function fetchPoolDetail(gachaId: string) {
-  const api = `https://proxy.viki.moe/hk4e/gacha_info/cn_gf01/${gachaId}/zh-cn.json?proxy-host=webstatic.mihoyo.com`
-  return await got.get(api).json<PoolDetailRes>()
+	const api = `https://webstatic.mihoyo.com/hk4e/gacha_info/cn_gf01/${gachaId}/zh-cn.json`;
+	return await got.get(api).json<PoolDetailRes>();
 }
 
 export interface PoolRes {
-  data: PoolData
-  message: string
-  retcode: number
+	data: PoolData;
+	message: string;
+	retcode: number;
 }
 
 export interface PoolData {
-  list: PoolItem[]
+	list: PoolItem[];
 }
 
 export interface PoolItem {
-  begin_time: string
-  end_time: string
-  gacha_id: string
-  gacha_name: string
-  gacha_type: number
+	begin_time: string;
+	end_time: string;
+	gacha_id: string;
+	gacha_name: string;
+	gacha_type: number;
 }
 
 export interface PoolDetailRes {
-  banner: string
-  content: string
-  date_range: string
-  gacha_type: number
-  r3_baodi_prob: string
-  r3_prob: string
-  r3_prob_list: ProbItem[]
-  r4_baodi_prob: string
-  r4_prob: string
-  r4_prob_list: ProbItem[]
-  r4_up_items: null | UpItem[]
-  r4_up_prob: string
-  r5_baodi_prob: string
-  r5_prob: string
-  r5_prob_list: ProbItem[]
-  r5_up_items: null | UpItem[]
-  r5_up_prob: string
-  title: string
+	banner: string;
+	content: string;
+	date_range: string;
+	gacha_type: number;
+	r3_baodi_prob: string;
+	r3_prob: string;
+	r3_prob_list: ProbItem[];
+	r4_baodi_prob: string;
+	r4_prob: string;
+	r4_prob_list: ProbItem[];
+	r4_up_items: null | UpItem[];
+	r4_up_prob: string;
+	r5_baodi_prob: string;
+	r5_prob: string;
+	r5_prob_list: ProbItem[];
+	r5_up_items: null | UpItem[];
+	r5_up_prob: string;
+	title: string;
 }
 
 export interface ProbItem {
-  is_up: number
-  item_id: number
-  item_name: string
-  item_type: string
-  order_value: number
-  rank: number
+	is_up: number;
+	item_id: number;
+	item_name: string;
+	item_type: string;
+	order_value: number;
+	rank: number;
 }
 
 export interface UpItem {
-  item_attr: string
-  item_color: string
-  item_id: number
-  item_img: string
-  item_name: string
-  item_type: string
-  item_type_cn: string
+	item_attr: string;
+	item_color: string;
+	item_id: number;
+	item_img: string;
+	item_name: string;
+	item_type: string;
+	item_type_cn: string;
 }
